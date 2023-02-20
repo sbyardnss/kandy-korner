c619f7912e4da3eafcd88cdac16f90f40c73a4bc
Mary Williams <sbyardnss@gmail.com>
2023-02-20 11:56:11 -0600

    adding top price filter button

--- src/components/products/products.js
+++ src/components/products/products.js
@@ -3,7 +3,10 @@ import { Navigate, useNavigate } from "react-router-dom"
 import "./products.css"
 
 export const ProductList = () => {
-    const [products, setProducts] = useState([])
+    const [products, setProducts] = useState([]);
+    const [topPriced, setTopPriced] = useState(false)
+    const [filteredProducts, setFiltered] = useState([])
+    
 
     const localKandyUser = localStorage.getItem("kandy_user")
     const kandyUserObject = JSON.parse(localKandyUser)
@@ -18,6 +21,29 @@ export const ProductList = () => {
         },
         []
     )
+    
+    useEffect(
+        () => {
+            if (topPriced) {
+                const expensiveProducts = products.filter(p => p.price >= 2)
+                setFiltered(expensiveProducts)
+            } 
+            else {
+                setFiltered(products)
+            }
+        },
+        [topPriced]
+    )
+    
+    useEffect(
+        () => {
+           
+            setFiltered(products)
+        },
+        [products]
+    )
+        
+    const alphabeticalProducts = filteredProducts.sort((a, b) => a.name < b.name ? -1 : a.name > b.name ? 1 : 0)
     return (
 
         <>
@@ -26,9 +52,10 @@ export const ProductList = () => {
                 kandyUserObject.staff ?
                     <>
                         <h2>Products</h2>
+                        <button onClick={() => setTopPriced(true) }>Top Priced</button>
                         <article className="products">
                             {
-                                products.map(
+                                alphabeticalProducts.map(
                                     (product) => {
                                         return <section className="product">
                                             <header>{product.name}</header>
