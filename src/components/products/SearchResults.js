fb0e2ebaa0a4d2f38f8fbb90c4c16462cff5f30b
Mary Williams <sbyardnss@gmail.com>
2023-02-23 10:18:56 -0600

    candy search and location alerts created

--- src/components/products/SearchResults.js
+++ src/components/products/SearchResults.js
@@ -1,15 +1,28 @@
 import { useEffect, useState } from "react"
-import { Navigate, useNavigate } from "react-router-dom"
+import { Link, Navigate, useNavigate } from "react-router-dom"
 import "./products.css"
 
-export const SearchResults = ({searchTermsState}) => {
+export const SearchResults = ({ searchTermsState }) => {
 
     const [products, setProducts] = useState([]);
     const [filteredProducts, setFiltered] = useState([])
+    const [locationProducts, setLocationProducts] = useState([])
     const navigate = useNavigate()
 
     const localKandyUser = localStorage.getItem("kandy_user")
     const kandyUserObject = JSON.parse(localKandyUser)
+
+
+    useEffect(
+        () => {
+            fetch(`http://localhost:8088/locationProducts?_expand=location`)
+                .then(response => response.json())
+                .then((lpArray) => {
+                    setLocationProducts(lpArray)
+                })
+        },
+        []
+    )
 
     useEffect(
         () => {
@@ -35,22 +48,43 @@ export const SearchResults = ({searchTermsState}) => {
 
 
 
-    
+
+
 
     const alphabeticalProducts = filteredProducts.sort((a, b) => a.name < b.name ? -1 : a.name > b.name ? 1 : 0)
 
 
     return <article>
         {
-        alphabeticalProducts.map(
-            (product) => {
-                return <section className="product">
-                    <header>{product.name}</header>
-                    <footer>{Intl.NumberFormat('en-US', { style: 'currency', currency: 'usd' }).format((product.price) / 100)}</footer>
-                </section>
-            }
-        )
-    }
+            alphabeticalProducts.map(
+                (product) => {
+                    const singleProductLocations = locationProducts.filter(lp => {
+                        return product.id === lp.productId
+                    })
+                    const productLocations = singleProductLocations.map(sPL => {
+                        return sPL?.location
+                    })
+
+                    return <section className="product">
+                        <header>{product.name}</header>
+                        <Link onClick={() => {
+                            alert(`Available at:
+                               ${
+                                    productLocations.map(pl => {
+                                        return `
+${pl.address}`
+                                    }).join("")
+                                }`
+
+                            )
+
+                        }
+                        }>Show me where!</Link>
+                        <footer>{Intl.NumberFormat('en-US', { style: 'currency', currency: 'usd' }).format((product.price) / 100)}</footer>
+                    </section>
+                }
+            )
+        }
     </article>
 }
 
